5459b692b4210e33bdf113048b2b1351a06fe37c
ricky <whdtjr2792@gmail.com>
2020-10-13 21:53:41 +0900

    4.3 Create Account Mutation part One

--- src/common/entities/core.entity.ts
+++ src/common/entities/core.entity.ts
@@ -1,12 +1,16 @@
 import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm/index';
+import { Field, ID } from '@nestjs/graphql';
 
 export class CoreEntity {
   @PrimaryGeneratedColumn()
+  @Field(type => ID)
   id: number;
 
   @CreateDateColumn()
+  @Field(type => Date)
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Field(type => Date)
   updatedAt: Date;
 }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,6 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+import { CreateAccountInput, CreateAccountOutput } from './dtos/create-account.dto';
 
 @Resolver(of => User)
 export class UsersResolver {
@@ -8,8 +9,14 @@ export class UsersResolver {
     private readonly usersService: UsersService,
   ) {
   }
+
   @Query(returns => Boolean)
   hi() {
     return true;
+  }
+
+  @Mutation(returns => CreateAccountOutput)
+  createAccount(@Args('input') createAccountInput: CreateAccountInput) {
+    //
   }
 }
