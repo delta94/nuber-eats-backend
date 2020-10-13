76d524ebd0963ec4ccf88319283973ed7726cc17
ricky <whdtjr2792@gmail.com>
2020-10-13 18:47:43 +0900

    2.5 Configuring ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,25 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+console.log(process.env.ENV);
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.ENV === 'prod',
+    }),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'postgres',
-        host: 'localhost',
-        port: 5435,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: +process.env.POSTGRES_PORT,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
         entities: [],
         logging: true,
         synchronize: true,
